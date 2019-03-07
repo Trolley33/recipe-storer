7adfa904a120a3b7606629cb7b68fa10054db2b8
Reece Trolley <reecetrolley@gmail.com>
2019-03-07 10:48:05 +0000

    Minor changes to imports/version numbers

--- app/src/main/java/com/example/recipekeeper/MainActivity.java
+++ app/src/main/java/com/example/recipekeeper/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.recipekeeper;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.WindowManager;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
