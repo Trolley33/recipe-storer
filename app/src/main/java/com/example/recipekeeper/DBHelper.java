d080ac6627689c0aee55bbd8b9524edfe71a6d54
Reece Trolley <reecetrolley@gmail.com>
2019-03-08 11:58:38 +0000

    Modify 'update' function to be more robust
    Changes raw query with a builder-style query.

--- app/src/main/java/com/example/recipekeeper/DBHelper.java
+++ app/src/main/java/com/example/recipekeeper/DBHelper.java
@@ -10,27 +10,36 @@ import android.util.Log;
 public class DBHelper extends SQLiteOpenHelper {
 
     static final String DATABASE_NAME = "recipe.db";
+
+    // For filtering favourites on the main screen.
     public enum FILTER {ALL, FAVOURITES};
 
 
     DBHelper(Context context) {
         super(context, DATABASE_NAME, null, 7);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
+    /**
+     * Creates tables for database.
+     * @param db database to affect.
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // Recipe table has id, name, overview, isFavourite, and ordering position.
         db.execSQL("CREATE TABLE recipes (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, OVERVIEW TEXT, FAVOURITE INT, POSITION INT)");
-
+        // Ingredients table has id, recipe_id, a description, an amount, and ordering position.
         db.execSQL("CREATE TABLE ingredients (ID INTEGER PRIMARY KEY AUTOINCREMENT, RECIPE_ID INT, DESCRIPTION TEXT, AMOUNT TEXT, POSITION INT)");
-
+        // Method table has id, recipe_id, an ordering position, some text, and a time for that step.
         db.execSQL("CREATE TABLE methods (ID INTEGER PRIMARY KEY AUTOINCREMENT, RECIPE_ID INT, POSITION INT, STEP TEXT, TIME REAL)");
-
+        // Category table has id, name.
         db.execSQL("CREATE TABLE categories (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
-
+        // Recipe + category table has id, recipe_id, category_id.
         db.execSQL("CREATE TABLE recipe_category (ID INTEGER PRIMARY KEY AUTOINCREMENT, RECIPE_ID INT, CATEGORY_ID INT)");
     }
 
+    /**
+     * When database is updated, remove all tables and recreate them.
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS recipes");
@@ -41,6 +50,12 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    /**
+     * When a new recipe is created (from the homescreen)
+     * @param name of recipe
+     * @param overview if supplied (usually blank).
+     * @return if row was inserted correctly.
+     */
     boolean createNewRecipe(String name, String overview)
     {
         SQLiteDatabase db = this.getWritableDatabase();
@@ -71,6 +86,11 @@ public class DBHelper extends SQLiteOpenHelper {
     {
         SQLiteDatabase db  = this.getWritableDatabase();
 
-        db.execSQL("UPDATE recipes SET NAME=?, OVERVIEW=?, FAVOURITE=?, POSITION=? WHERE ID=?", new Object[] {name, overview, favourite, position, id});
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NAME", name);
+        contentValues.put("OVERVIEW", overview);
+        contentValues.put("FAVOURITE", favourite);
+        contentValues.put("POSITION", position);
+        db.update("recipes", contentValues, "ID="+id, null);
     }
 }
