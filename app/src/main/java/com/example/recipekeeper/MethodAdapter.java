0176e78929d951a14d9b0c28b3dac0d4f5233875
Reece Trolley <reecetrolley@gmail.com>
2019-03-20 10:35:18 +0000

    Add method layouts, fragments, adapter

---
+++ app/src/main/java/com/example/recipekeeper/MethodAdapter.java
@@ -0,0 +1,161 @@
+package com.example.recipekeeper;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MethodAdapter extends RecyclerView.Adapter<MethodAdapter.ViewHolder>
+{
+    private List<Method> methods;
+    private RecipeMethodsFragment fragment;
+
+    public MethodAdapter(List<Method> _methods, RecipeMethodsFragment _fragment)
+    {
+        methods = _methods;
+        fragment = _fragment;
+    }
+
+    @NonNull
+    @Override
+    public MethodAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View methodView = inflater.inflate(R.layout.item_method, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(methodView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MethodAdapter.ViewHolder viewHolder, int i) {
+        final Method method = methods.get(i);
+
+        TextView stepTextView = viewHolder.stepTextView;
+        stepTextView.setText(method.getStep());
+
+        TextView timeTextView = viewHolder.timeTextView;
+        timeTextView.setText(String.format("%f", method.getTime()));
+
+        ConstraintLayout layout = viewHolder.layout;
+        final Context context = viewHolder.context;
+
+        Button edit_button = viewHolder.editButton;
+        Button delete_button = viewHolder.deleteButton;
+
+        edit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editMethod(viewHolder.itemView, method);
+            }
+        });
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteMethod(viewHolder.itemView, method);
+            }
+        });
+    }
+
+    public void editMethod(View view, final Method method)
+    {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle("Edit Method");
+
+        View v = View.inflate(view.getContext(), R.layout.add_method_popup, null);
+
+        builder.setView(v);
+
+        final EditText step_input = v.findViewById(R.id.step_edit);
+        final EditText time_input = v.findViewById(R.id.time_edit);
+
+        step_input.setText(method.getStep());
+        time_input.setText(String.format("%f", method.getTime()));
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                method.setStep(step_input.getText().toString());
+                method.setTime(Double.parseDouble(time_input.getText().toString()));
+                fragment.refreshMethods(null);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void deleteMethod(View view, final Method method)
+    {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle("Delete Method");
+
+        builder.setMessage("This cannot be undone.");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                method.delete();
+                fragment.refreshMethods(null);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    @Override
+    public int getItemCount() {
+        return methods.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ConstraintLayout layout;
+        TextView stepTextView;
+        TextView timeTextView;
+
+        Button editButton;
+        Button deleteButton;
+
+        Context context;
+
+        public ViewHolder(final View itemView) {
+
+            super(itemView);
+            context = itemView.getContext();
+
+            layout =  itemView.findViewById(R.id.layout);
+            stepTextView = itemView.findViewById(R.id.method_step);
+            timeTextView = itemView.findViewById(R.id.method_time);
+
+            editButton = itemView.findViewById(R.id.method_edit_button);
+            deleteButton = itemView.findViewById(R.id.method_delete_button);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/recipekeeper/RecipeMethodsFragment.java
@@ -0,0 +1,187 @@
+package com.example.recipekeeper;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+
+ * interface.
+ */
+public class RecipeMethodsFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private Recipe selectedRecipe;
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+    private RecyclerView recyclerView;
+    private List<Method> methodsList;
+    private MethodAdapter adapter;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public RecipeMethodsFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static RecipeMethodsFragment newInstance(int columnCount) {
+        RecipeMethodsFragment fragment = new RecipeMethodsFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void setSelectedRecipe(Recipe recipe)
+    {
+        selectedRecipe = recipe;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        methodsList = new ArrayList<>();
+        adapter = new MethodAdapter(methodsList, this);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View view = inflater.inflate(R.layout.fragment_method_list, container, false);
+        Context context = view.getContext();
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+
+            recyclerView = (RecyclerView) view;
+        }
+        else
+        {
+            recyclerView = view.findViewById(R.id.method_list);
+        }
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fab = view.findViewById(R.id.add_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addMethod(view);
+            }
+        });
+
+        refreshMethods(view);
+        adapter.notifyDataSetChanged();
+
+        attachItemTouchHelper();
+
+        return view;
+    }
+
+    void attachItemTouchHelper ()
+    {
+        // Extend the Callback class
+        ItemTouchHelper.Callback _ithCallback = new ItemTouchHelper.Callback() {
+            // When an item is dragged and dropped.
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                // Swap dragged item and target item.
+                methodsList.get(viewHolder.getAdapterPosition()).setPosition(target.getAdapterPosition());
+                methodsList.get(target.getAdapterPosition()).setPosition(viewHolder.getAdapterPosition());
+
+                Collections.swap(methodsList, viewHolder.getAdapterPosition(), target.getAdapterPosition());
+
+                // Notify adapter to change positions.
+                adapter.notifyItemMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                //TODO
+            }
+
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                return makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,
+                        ItemTouchHelper.DOWN | ItemTouchHelper.UP | ItemTouchHelper.START | ItemTouchHelper.END);
+            }
+        };
+        // Create an `ItemTouchHelper` and attach it to the `RecyclerView`
+        ItemTouchHelper ith = new ItemTouchHelper(_ithCallback);
+        ith.attachToRecyclerView(recyclerView);
+    }
+
+    void refreshMethods(View view)
+    {
+        methodsList.clear();
+        methodsList.addAll(Method.getMethodList(selectedRecipe.getID()));
+        adapter.notifyDataSetChanged();
+    }
+
+    public void addMethod(View view)
+    {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Add new ingredient");
+
+        View v = getLayoutInflater().inflate(R.layout.add_method_popup, null);
+
+        builder.setView(v);
+
+        final EditText step_input = v.findViewById(R.id.step_edit);
+        final EditText time_input = v.findViewById(R.id.time_edit);
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Method.addMethod(selectedRecipe.getID(), step_input.getText().toString(), Double.parseDouble(time_input.getText().toString()));
+                refreshMethods(null);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
