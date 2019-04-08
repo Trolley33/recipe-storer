1631093985ae7663d25bf1fbbd50c3740ce5e085
Reece Trolley <reecetrolley@gmail.com>
2019-04-08 12:15:16 +0100

    Begin work on user guide webview/html page(s)
    Works toward #30.

--- app/src/main/java/com/example/recipekeeper/UserGuideActivity.java
+++ app/src/main/java/com/example/recipekeeper/UserGuideActivity.java
@@ -2,6 +2,9 @@ package com.example.recipekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -13,9 +16,31 @@ public class UserGuideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_guide);
 
-        webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com");
+        // Setup action bar
+        Toolbar toolbar = findViewById(R.id.action_bar);
+        toolbar.setTitle("User Guide");
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
