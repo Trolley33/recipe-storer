311b39b53919d2e3c99c5e950805ecb778f9f473
Reece Trolley <reecetrolley@gmail.com>
2019-04-28 19:01:15 +0100

    Add custom notification view
    Adds buttons to notification, as well as space for a step and the time.

--- app/src/main/java/com/example/recipekeeper/TimerService.java
+++ app/src/main/java/com/example/recipekeeper/TimerService.java
@@ -4,11 +4,13 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -27,6 +29,8 @@ public class TimerService extends Service {
     private NotificationManagerCompat notificationManager;
     private boolean firstTime = true;
 
+    RemoteViews controls;
+
     int secondsRemaining;
 
     @Override
@@ -39,6 +43,37 @@ public class TimerService extends Service {
     public void onCreate() {
         builder = new Notification.Builder(this);
         notificationManager = NotificationManagerCompat.from(this);
+        controls = new RemoteViews(this.getPackageName(), R.layout.timer_notification);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        if (isRunning)
+            return;
+
+        isRunning = true;
+        secondsRemaining = intent.getExtras().getInt("SECONDS");
+
+        // Delete previous notification
+        notificationManager.cancel(0);
+        controls.setTextViewText(R.id.step_text, "Step: blah blah");
+        controls.setTextViewText(R.id.time_text, "Time Remaining: 01:12");
+
+        controls.setImageViewResource(R.id.previous_step, R.drawable.ic_baseline_skip_previous_24px);
+        controls.setImageViewResource(R.id.play_pause, R.drawable.ic_baseline_pause_24px);
+        controls.setImageViewResource(R.id.next_step, R.drawable.ic_baseline_skip_next_24px);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (secondsRemaining > 0) {
+                    sendTimerNotification(--secondsRemaining);
+                    SystemClock.sleep(1000);
+                }
+                stopSelf();
+            }
+        }).start();
     }
 
     public void sendTimerNotification(int seconds) {
@@ -46,8 +81,10 @@ public class TimerService extends Service {
             if (firstTime) {
                 builder
                         .setChannelId("channel")
+                        .setCustomContentView(controls)
+                        .setStyle(new Notification.DecoratedCustomViewStyle())
                         .setContentTitle("Time Remaining")
-                        .setSmallIcon(R.drawable.ic_baseline_hourglass_empty_24px) // possibly replace with hourglass?
+                        .setSmallIcon(R.drawable.ic_baseline_hourglass_empty_24px)
                         .setOnlyAlertOnce(true);
                 firstTime = false;
             }
@@ -70,37 +107,15 @@ public class TimerService extends Service {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
                 Notification notification = builder
                         .setChannelId("channel")
+                        .setCustomContentView(null)
                         .setContentTitle("Recipe Storer")
-                        .setSmallIcon(R.drawable.ic_baseline_hourglass_empty_24px) // possibly replace with hourglass?
+                        .setSmallIcon(R.drawable.ic_baseline_hourglass_empty_24px)
                         .setOnlyAlertOnce(false)
                         .setContentText("Timer Complete!")
                         .build();
 
             notificationManager.notify(0, notification);
         }
-    }
-
-    @Override
-    public void onStart(Intent intent, int startId) {
-        if (isRunning)
-            return;
-
-        isRunning = true;
-        secondsRemaining = intent.getExtras().getInt("SECONDS");
-
-        // Delete previous notification
-        notificationManager.cancel(0);
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (secondsRemaining > 0) {
-                    sendTimerNotification(--secondsRemaining);
-                    SystemClock.sleep(1000);
-                }
-                stopSelf();
-            }
-        }).start();
     }
 
     @Override
