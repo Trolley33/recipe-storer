cd18285d969a21746be3f5c230879d16e454dfc1
Reece Trolley <reecetrolley@gmail.com>
2019-05-03 12:15:47 +0100

    Comment SearchRecipesFragment
    Closes #73.

--- app/src/main/java/com/example/recipekeeper/SearchRecipesFragment.java
+++ app/src/main/java/com/example/recipekeeper/SearchRecipesFragment.java
@@ -2,6 +2,7 @@ package com.example.recipekeeper;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,19 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A fragment representing a list of Items.
- * <p/>
- * <p>
- * interface.
+ * A fragment for listing recipes.
  */
 public class SearchRecipesFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
-    private RecyclerView recyclerView;
     private List<Recipe> recipeList;
     private RecipeAdapter adapter;
 
@@ -37,34 +29,36 @@ public class SearchRecipesFragment extends Fragment {
     public SearchRecipesFragment() {
     }
 
+    /**
+     * Called when fragment is created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Instantiate method variables.
         recipeList = new ArrayList<>();
         adapter = new RecipeAdapter(recipeList);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        // Inflate view with layout.
         final View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
         Context context = view.getContext();
 
-        recyclerView = view.findViewById(R.id.recipe_list);
+        // Get recycler view and bind adapter to it.
+        RecyclerView recyclerView = view.findViewById(R.id.recipe_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
         recyclerView.setAdapter(adapter);
 
+        // Refresh recipe list, no method needed as this only happens once per search.
         recipeList.clear();
         recipeList.addAll(Recipe.searchFor(((SearchActivity) getActivity()).getSearchTerm()));
         adapter.notifyDataSetChanged();
 
-        // Hide floating action button from view.
+        // Hide included floating action button from view.
         FloatingActionButton fab = view.findViewById(R.id.edit_fab);
         fab.hide();
 
