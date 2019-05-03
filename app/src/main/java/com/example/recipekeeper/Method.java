b7c76aa7c165a372c5a7821b4519a8ba9b6c7a2f
Reece Trolley <reecetrolley@gmail.com>
2019-05-03 11:22:20 +0100

    Comment Method
    Closes #64.

--- app/src/main/java/com/example/recipekeeper/Method.java
+++ app/src/main/java/com/example/recipekeeper/Method.java
@@ -11,9 +11,9 @@ public class Method {
     private int position;
     private String step;
     private double time;
-    private ArrayList<Category> categories;
 
     public Method(int _id, int _recipe_id, int _pos, String _step, double _time) {
+        // Set id and category name on instantiation.
         id = _id;
         recipe_id = _recipe_id;
         position = _pos;
@@ -21,22 +21,38 @@ public class Method {
         time = _time;
     }
 
-    public static void addMethod(int recipe_id, String step, double time) {
+    /**
+     * Add new step to method in database.
+     * @param recipe_id of recipe to add to.
+     * @param step description
+     * @param time in minutes.
+     */
+    static void addMethod(int recipe_id, String step, double time) {
         db.createNewMethod(recipe_id, getMethodList(recipe_id).size(), step, time);
     }
 
-    public static ArrayList<Method> getMethodList(int recipe_id) {
+    /**
+     * Retrieves a list of all steps for a recipe.
+     * @param recipe_id to get steps for.
+     * @return {@link ArrayList<Method>} of steps.
+     */
+    static ArrayList<Method> getMethodList(int recipe_id) {
         ArrayList<Method> methods = new ArrayList<>();
 
+        // Retrieve cursor containing all steps from  database.
         Cursor res = db.getMethodList(recipe_id);
+        // Query failed, return blank list.
         if (res == null) {
             return methods;
         }
+        // Query empty, return blank list.
         if (res.getCount() == 0) {
             return methods;
         }
 
+        // Loop over each row.
         while (res.moveToNext()) {
+            // Add Method object to list.
             int _id = res.getInt(0);
             int _recipe_id = res.getInt(1);
             int _position = res.getInt(2);
@@ -46,58 +62,55 @@ public class Method {
             methods.add(new Method(_id, _recipe_id, _position, _step, _time));
         }
 
-
+        // Return list.
         return methods;
-    }
-
-    public static Method getFromID(int _id, int recipe_id) {
-        for (Method i : getMethodList(recipe_id)) {
-            if (i.getID() == _id) {
-                return i;
-            }
-        }
-        return null;
     }
 
     public int getID() {
         return id;
     }
 
-    public int getRecipeID() {
+    private int getRecipeID() {
         return recipe_id;
     }
 
-    public int getPosition() {
+    int getPosition() {
         return position;
     }
 
-    public void setPosition(int _pos) {
+    void setPosition(int _pos) {
         position = _pos;
         updateMethod();
     }
 
-    public String getStep() {
+    String getStep() {
         return step;
     }
 
-    public void setStep(String _step) {
+    void setStep(String _step) {
         step = _step;
         updateMethod();
     }
 
-    public double getTime() {
+    double getTime() {
         return time;
     }
 
-    public void setTime(double _time) {
+    void setTime(double _time) {
         time = _time;
         updateMethod();
     }
 
-    void updateMethod() {
+    /**
+     * Update database side information with this step.
+     */
+    private void updateMethod() {
         db.updateMethod(getID(), getRecipeID(), getPosition(), getStep(), getTime());
     }
 
+    /**
+     * Delete this step from the database.
+     */
     public void delete() {
         db.deleteMethod(getID());
     }
