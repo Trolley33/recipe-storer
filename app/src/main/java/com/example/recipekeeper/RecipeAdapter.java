ea893e4dc8a0aea728af84d6430cd3b2dc260003
Reece Trolley <reecetrolley@gmail.com>
2019-05-03 12:09:21 +0100

    Comment RecipeAdapter
    Closes #68.

--- app/src/main/java/com/example/recipekeeper/RecipeAdapter.java
+++ app/src/main/java/com/example/recipekeeper/RecipeAdapter.java
@@ -16,39 +16,49 @@ import java.util.List;
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
     private List<Recipe> recipes;
 
-    public RecipeAdapter(List<Recipe> _recipes) {
+    RecipeAdapter(List<Recipe> _recipes) {
         recipes = _recipes;
     }
 
+    /**
+     * Called when view holder is created.
+     */
     @NonNull
     @Override
     public RecipeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
+        // Inflate view with custom layout.
         View recipeView = inflater.inflate(R.layout.item_recipe, parent, false);
 
-        ViewHolder viewHolder = new ViewHolder(recipeView);
-
-        return viewHolder;
+        return new ViewHolder(recipeView);
     }
 
+    /**
+     * When this is bound to it's parent.
+     */
     @Override
     public void onBindViewHolder(@NonNull final RecipeAdapter.ViewHolder viewHolder, int i) {
+        // Get the recipe represented by this adapter.
         final Recipe recipe = recipes.get(i);
 
+        // Retrieve UI elements from parent.
         TextView textView = viewHolder.nameTextView;
-        textView.setText(recipe.getName());
         Button favouriteButton = viewHolder.favouriteButton;
         ConstraintLayout layout = viewHolder.layout;
         final Context context = viewHolder.context;
 
+        // Set value of text view to match recipe information.
+        textView.setText(recipe.getName());
+
+        // Set correct icon for favourite star.
         if (recipe.isFavourite()) {
             favouriteButton.setBackgroundResource(R.drawable.ic_baseline_star_24px);
         } else {
             favouriteButton.setBackgroundResource(R.drawable.ic_baseline_star_border_24px);
         }
-
+        // Bind favourite button click event to togggle recipe's favourite state, and update that recipe in the adapter.
         favouriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,11 +66,12 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
                 notifyItemChanged(recipe.getPosition());
             }
         });
+        // Bind row click event to start intent for this recipe, and open the new activity.
         layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, RecipeActivity.class);
-
+                // Place recipe id as extra, to retrieve later.
                 intent.putExtra(HomeActivity.RECIPE_ID_MESSAGE, recipe.getID());
                 context.startActivity(intent);
             }
@@ -68,24 +79,31 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
 
     }
 
+    /**
+     * Number of items this adapter will display.
+     * @return size of recipe list.
+     */
     @Override
     public int getItemCount() {
         return recipes.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    /**
+     * Sets member variables for each view holder.
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ConstraintLayout layout;
         TextView nameTextView;
         Button favouriteButton;
-        ConstraintLayout layout;
         Context context;
 
-        public ViewHolder(final View itemView) {
+        ViewHolder(final View itemView) {
 
             super(itemView);
+            layout = itemView.findViewById(R.id.layout);
+            nameTextView = itemView.findViewById(R.id.recipe_name);
+            favouriteButton = itemView.findViewById(R.id.fav_button);
             context = itemView.getContext();
-            layout = (ConstraintLayout) itemView.findViewById(R.id.layout);
-            nameTextView = (TextView) itemView.findViewById(R.id.recipe_name);
-            favouriteButton = (Button) itemView.findViewById(R.id.fav_button);
         }
     }
 }
